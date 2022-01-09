09e59e4393e233cab5c18a2bea370134ca24f935
Anastasiia Dmytrenko <admytrenko@ODL1806015.local>
2022-01-09 12:12:13 +0200

    kth_largest solution

---
+++ kth_largest.py
@@ -0,0 +1,23 @@
+import doctest
+
+
+def kth_largest(numbers, k):
+    """
+    See https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+    >>> kth_largest([3, 2, 1, 5, 6, 4], 2)
+    5
+    >>> kth_largest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)
+    4
+    >>> kth_largest([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)
+    9
+    >>> kth_largest([10, 9, 5, 6, 4, 7, 2, 1, 3, 8], 3)
+    8
+    """
+
+    sorted_numbers = sorted(numbers, reverse=True)
+    return sorted_numbers[k - 1]
+
+
+if __name__ == "__main__":
+    doctest.testmod(verbose=True)
